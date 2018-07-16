963f4370f9991f1eb8908eaec73ba52111fec10e
Foster Stule <camodude5@yahoo.com>
2018-07-16 15:38:50 -0400

    integrated cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { UserCreateComponent } from './user/user-create/user-create.component';
     UserListComponent,
     UserDetailComponent,
     UserEditComponent,
-    UserCreateComponent
+    UserCreateComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,FormsModule
