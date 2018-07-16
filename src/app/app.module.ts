dab284c3353e8eba74e6f2335844bc64bfce826b
Foster Stule <camodude5@yahoo.com>
2018-07-16 11:01:41 -0400

    changed it a little

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeCompComponent } from './home-comp/home-comp.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserCreateComponent } from './user/user-create/user-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCompComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserEditComponent,
+    UserCreateComponent
   ],
   imports: [
     BrowserModule
